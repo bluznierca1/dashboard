e000a665db72d402c1eeb122c35bcc6e60238897
Greg Lasak <gla@.mindworking.dk>
2020-12-18 15:35:53 +0100

    Added some dev/live info to config files visible in GitHub

--- config.php
+++ config.php
@@ -1,17 +1,26 @@
 <?php
 
+/*
+ * That file would be excluded from GIT but that project is not going live.
+ * Even if that happens, it will look different :)
+ */
+
 date_default_timezone_set('Europe/Copenhagen');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+!defined('IS_DEV') ? define('IS_DEV', true) : null;
 
-!defined('URL_PREFIX') ? define('URL_PREFIX', 'dashboard') : null;
+if( IS_DEV ) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
+!defined('URL_PREFIX') ? define('URL_PREFIX', IS_DEV ? 'dashboard' : 'SOME_OTHER_PREFIX_OR_NONE') : null;
 !defined('APP_ROOT') ? define('APP_ROOT', __DIR__ ) : null;
 
 !defined( 'SRC_ROOT' ) ? define('SRC_ROOT', APP_ROOT . '/src/') : null;
 !defined( 'TEMPLATES_PATH' ) ? define('TEMPLATES_PATH', APP_ROOT . '/templates/') : null;
-!defined('HOME_URL') ? define('HOME_URL', 'http://localhost:8080/dashboard') : null;
+!defined('HOME_URL') ? define('HOME_URL', IS_DEV ? 'http://localhost:8080/dashboard' : 'SOME_OTHER_LINK') : null;
 
 spl_autoload_register( static function($class) {
 
